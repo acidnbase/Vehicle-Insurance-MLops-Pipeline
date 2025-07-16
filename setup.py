1754e3c01ba1d2a4bf860a893f9c0ebeb6c972b9
Keshav <jhakeshav261@gmail.com>
2025-07-16 19:31:21 +0530

    project structure ready, venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Keshav",
+    author_email="jhakeshav261@gmail.com",
+    packages=find_packages(),
+)
